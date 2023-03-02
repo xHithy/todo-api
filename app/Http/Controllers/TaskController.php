a34fde30814cc089d0375ccd6b32ace9933b8dc8
Hithy <55604744+xHithy@users.noreply.github.com>
2023-03-03 00:43:45 +0200

    API endpoints and migrations
    Created the base API endpoints
    
    Created migrations for the todo/tasks table

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class TaskController extends Controller
+{
+    public static function fetchTodos($limit)
+    {
+
+    }
+
+    public static function createTodo()
+    {
+
+    }
+
+    public static function updateTodo()
+    {
+
+    }
+
+    public static function deleteTodo()
+    {
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,12 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('v1/todos')->middleware('verify.admin')->group(function() {
+    Route::get('/{?limit}', [TaskController::class, 'fetchTodos']);
+    Route::post('', [TaskController::class, 'createTodo']);
+    Route::put('/{id}', [TaskController::class, 'updateTodo']);
+    Route::delete('/{id}', [TaskController::class, 'deleteTodo']);
 });
